6e162c12e096532abe7cb16ba9a9f1eb9f44307a
AdriaSeSa <76749150+AdriaSeSa@users.noreply.github.com>
2022-04-29 00:37:53 +0200

    First try on FMPeg

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,56 +1,213 @@
-#include "opencv2/highgui/highgui.hpp"
 
-#include "libavcodec/avcodec.h"
+#include "libavcodec/avcodec.h"  
+#include "libavformat/avformat.h"  
+#include "libswscale/swscale.h"  
+#include "libavutil/imgutils.h"  
+#include "SDL.h"  
 
-#include <iostream>
-
-using namespace cv;
-using namespace std;
+#include <memory>
 
 int main(int argc, char** argv)
 {
-    VideoCapture cap("video.mp4");
-    // cap is the object of class video capture that tries to capture Bumpy.mp4
-    if (!cap.isOpened())  // isOpened() returns true if capturing has been initialized.
-    {
-        cout << "Cannot open the video file. \n";
-        return -1;
-    }
+	// Run code to play a video using OpenCV
+	//OpenCVVideoPlayer();
 
-    double fps = cap.get(cv::CAP_PROP_FPS); //get the frames per seconds of the video
-    // The function get is used to derive a property from the element.
-    // Example:
-    // CV_CAP_PROP_POS_MSEC :  Current Video capture timestamp.
-    // CV_CAP_PROP_POS_FRAMES : Index of the next frame.
+	// Initalizing to NULL prevents segfaults!
+	AVFormatContext* pFormatCtx = NULL;
+	// Open video file
+	if (avformat_open_input(&pFormatCtx, "video.mp4", NULL, NULL) != 0)
+	{
+		av_log(NULL, AV_LOG_ERROR, "Cannot open input file\n");
+		return -1; // Couldn't open file
+	}
+	// Dump information about file onto standard error
+	av_dump_format(pFormatCtx, 0, "1=========================>test.mp4", 0);
+	// Retrieve stream information
+	if (avformat_find_stream_info(pFormatCtx, NULL) < 0)
+	{
+		av_log(NULL, AV_LOG_ERROR, "Cannot find stream information\n");
+		return -1; // Couldn't find stream information
+	}
+	// Contraste El último volcado puede ver la información que llena la transmisión en AVFFormatContext a través de la función AVFORMAT_FIND_STREAM_INFO
+	av_dump_format(pFormatCtx, 0, "2=========================>test.mp4", 0);
 
-    namedWindow("A_good_name", WINDOW_AUTOSIZE); //create a window called "MyVideo"
-    // first argument: name of the window.
-    // second argument: flag- types: 
-    // WINDOW_NORMAL : The user can resize the window.
-    // WINDOW_AUTOSIZE : The window size is automatically adjusted to fitvthe displayed image() ), and you cannot change the window size manually.
-    // WINDOW_OPENGL : The window will be created with OpenGL support.
+	const AVCodec* pDec = NULL;
+	/* select the video stream and find the decoder*/
+	int video_stream_index = av_find_best_stream(pFormatCtx, AVMEDIA_TYPE_VIDEO, -1, -1, &pDec, 0);
+	if (video_stream_index < 0)
+	{
+		av_log(NULL, AV_LOG_ERROR, "Cannot find a video stream in the input file\n");
+		return -1;
+	}
 
-    while (1)
-    {
-        Mat frame;
-        // Mat object is a basic image container. frame is an object of Mat.
+	//Note that we must not use the AVCodecContext from the video stream directly! 
+	//So we have to use avcodec_copy_context() to copy the context to a new location (after allocating memory for it, of course).	
+	/* create decoding context */
+	AVCodecContext* pDecCtx = avcodec_alloc_context3(pDec);
+	if (!pDecCtx)
+	{
+		return AVERROR(ENOMEM);
+	}
 
-        if (!cap.read(frame)) // if not success, break loop
-        // read() decodes and captures the next frame.
-        {
-            cout << "\n Cannot read the video file. \n";
-            break;
-        }
+	//Fill the codec context based on the values from the supplied codec
+	avcodec_parameters_to_context(pDecCtx, pFormatCtx->streams[video_stream_index]->codecpar);
 
-        imshow("A_good_name", frame);
-        // first argument: name of the window.
-        // second argument: image to be shown(Mat object).
+	//Initialize the AVCodecContext to use the given AVCodec.
+	if (avcodec_open2(pDecCtx, pDec, NULL) < 0)
+	{
+		av_log(NULL, AV_LOG_ERROR, "Cannot open video decoder\n");
+		return -1;
+	}
 
-        if (waitKey(30) == 27) // Wait for 'esc' key press to exit
-        {
-            break;
-        }
-    }
 
-    return 0;
+	AVPacket* packetData = av_packet_alloc();
+	//Allocate an AVFrame and set its fields to default values.
+	AVFrame* pFrame = av_frame_alloc();
+	AVFrame* pFrameYUV = av_frame_alloc();
+
+	// Determine required buffer size and allocate buffer
+	int byteSize = av_image_get_buffer_size(AV_PIX_FMT_YUV420P,
+		pDecCtx->width,
+		pDecCtx->height,
+		1
+	);
+	uint8_t* buffer = (uint8_t*)av_malloc(byteSize);
+
+	// Assign appropriate parts of buffer to image planes in pFrameRGB
+	// Note that pFrameRGB is an AVFrame, but AVFrame is a superset
+	// of AVPicture
+	av_image_fill_arrays(pFrameYUV->data, pFrameYUV->linesize,
+		buffer, AV_PIX_FMT_YUV420P,
+		pDecCtx->width,
+		pDecCtx->height,
+		1
+	);
+
+	int screen_w = pDecCtx->width / 2;
+	int screen_h = pDecCtx->height / 2;
+	// initialize SWS context for software scaling
+	SwsContext* pSws_ctx = sws_getContext(pDecCtx->width,
+		pDecCtx->height,
+		pDecCtx->pix_fmt,
+		screen_w,
+		screen_h,
+		AV_PIX_FMT_YUV420P,
+		SWS_BICUBIC,
+		NULL,
+		NULL,
+		NULL
+	);
+
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER))
+	{
+		printf("Could not initialize SDL - %s\n", SDL_GetError());
+		return -1;
+	}
+
+	//SDL 2.0 Support for multiple windows  
+	SDL_Window* screen = SDL_CreateWindow("SDL",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		screen_w, screen_h,
+		SDL_WINDOW_OPENGL
+	);
+
+	if (!screen)
+	{
+		printf("SDL: could not create window - exiting:%s\n", SDL_GetError());
+		return -1;
+	}
+
+	SDL_Renderer* sdlRenderer = SDL_CreateRenderer(screen, -1, 0);
+	//IYUV: Y + U + V  (3 planes)  
+	SDL_Texture* sdlTexture = SDL_CreateTexture(sdlRenderer,
+		SDL_PIXELFORMAT_IYUV,
+		SDL_TEXTUREACCESS_STREAMING,
+		screen_w,
+		screen_h
+	);
+	SDL_Rect sdlRect;
+	sdlRect.x = 0;
+	sdlRect.y = 0;
+	sdlRect.w = screen_w;
+	sdlRect.h = screen_h;
+
+	int ret = -1;
+	SDL_Event event;
+
+	while (1)
+	{
+		/**
+		*Technically a packet can contain partial frames or other bits of data,
+		*but ffmpeg's parser ensures that the packets we get contain either complete or multiple frames.
+		*/
+		ret = av_read_frame(pFormatCtx, packetData);
+		if (ret < 0)
+		{
+			break;
+		}
+
+		if (packetData->stream_index == video_stream_index)
+		{
+			//Supply raw packet data as input to a decoder.
+			ret = avcodec_send_packet(pDecCtx, packetData);
+			if (ret < 0)
+			{
+				av_log(NULL, AV_LOG_ERROR, "Error while sending a packet to the decoder\n");
+				break;
+			}
+
+			while (ret >= 0)
+			{
+				ret = avcodec_receive_frame(pDecCtx, pFrame);
+				//Return decoded output data from a decoder.
+				// Un paquete puede tener múltiples marcos
+				if (ret >= 0)
+				{
+					// Convert the image from its native format to YUV
+					sws_scale(pSws_ctx,
+						(const uint8_t* const*)pFrame->data,
+						pFrame->linesize,
+						0,
+						pDecCtx->height,
+						pFrameYUV->data,
+						pFrameYUV->linesize
+					);
+
+				}
+				else if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+				{
+					break;
+				}
+				else
+				{
+					av_log(NULL, AV_LOG_ERROR, "Error while receiving a frame from the decoder\n");
+					return -1;
+				}
+
+				SDL_UpdateYUVTexture(sdlTexture, &sdlRect,
+					pFrameYUV->data[0], pFrameYUV->linesize[0],
+					pFrameYUV->data[1], pFrameYUV->linesize[1],
+					pFrameYUV->data[2], pFrameYUV->linesize[2]
+				);
+				SDL_RenderClear(sdlRenderer);
+				SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, NULL);
+				SDL_RenderPresent(sdlRenderer);
+			}
+
+		}
+		// Wipe the packet.		
+		av_packet_unref(packetData);
+		SDL_PollEvent(&event);
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			SDL_Quit();
+			break;
+		default:
+			break;
+		}
+
+		return 0;
+	}
 }
---
+++ Source/OpenCVVideoPlayer.h
@@ -0,0 +1,7 @@
+#pragma once
+class OpenCVVideoPlayer
+{
+public:
+	OpenCVVideoPlayer();
+};
+
